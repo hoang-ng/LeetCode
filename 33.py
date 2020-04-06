efbc325719b692f418d7c706a7f34f106add3e2c
Hoang Nguyen <hoang2109@gmail.com>
2020-04-06 23:55:41 +0800

    Solutions for [#33,#34,#1331]

---
+++ 33.py
@@ -0,0 +1,35 @@
+# 33. Search in Rotated Sorted Array
+
+# Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+# (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+# You are given a target value to search. If found in the array return its index, otherwise return -1.
+# You may assume no duplicate exists in the array.
+# Your algorithm's runtime complexity must be in the order of O(log n).
+
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+
+class Solution:
+    def search(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[l] <= nums[mid]:
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
---
+++ 34.py
@@ -0,0 +1,76 @@
+# 34. Find First and Last Position of Element in Sorted Array
+
+# Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+# Your algorithm's runtime complexity must be in the order of O(log n).
+# If the target is not found in the array, return [-1, -1].
+
+# Example 1:
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+
+# Example 2:
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+
+class Solution1:
+    def searchRange(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                l = mid - 1
+                r = mid + 1
+                while l >= 0:
+                    if target == nums[l]:
+                        l -= 1
+                    else:
+                        break
+                while r < len(nums):
+                    if target == nums[r]:
+                        r += 1
+                    else:
+                        break
+                return [l + 1, r - 1]
+            elif target > nums[mid]:
+                l = mid + 1
+            else:
+                r = mid + 1
+        return [-1, -1]
+
+class Solution2:
+    def searchRange(self, A, target):
+        lmost = self.leftsearch(A,target)
+        rmost = self.rightsearch(A,target)
+        return[lmost,rmost]
+
+    def leftsearch(self,A,tar):
+        l = 0
+        r = len(A) - 1
+        rs = -1
+        while l <= r:
+            mid = (l + r)/2
+            if A[mid] > tar:
+                r = mid - 1
+            elif A[mid] < tar:
+                l = mid + 1
+            else:
+                rs = mid
+                r = mid - 1
+        return rs
+
+
+    def rightsearch(self,A,tar):
+        l = 0
+        r = len(A)-1
+        rs = -1
+        while l <= r:
+            mid = (l + r) / 2
+            if A[mid] > tar:
+                r = mid -1
+            elif A[mid] <tar:
+                l = mid + 1
+            else:
+                rs = mid
+                l = mid + 1
+        return rs
