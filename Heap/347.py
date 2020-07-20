5b1fb6e794f6e5e76c32a641de109f56b4081c97
Hoang Nguyen <hoang2109@gmail.com>
2020-07-20 21:52:16 +0800

    Solution #347

---
+++ Heap/347.py
@@ -0,0 +1,36 @@
+# 347. Top K Frequent Elements
+
+# Given a non-empty array of integers, return the k most frequent elements.
+
+# Example 1:
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+
+# Example 2:
+# Input: nums = [1], k = 1
+# Output: [1]
+
+# Note:
+# You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+# Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+# It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+# You can return the answer in any order.
+
+import heapq
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        dic = {}
+        for n in nums:
+            dic[n] = dic.get(n, 0) + 1
+
+        heap = []
+        for key, value in dic.items():
+            heap.append((-value, key))
+        heapq.heapify(heap)
+        rs = []
+        for _ in range(k):
+            rs.append(heapq.heappop(heap)[1])
+
+        return rs
