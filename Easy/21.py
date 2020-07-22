34ed3c298be4581c22a840aec35a6992761e86b2
Hoang Nguyen <hoang2109@gmail.com>
2020-07-22 22:05:58 +0800

    Solutions [#13,#20,#21,#88]

---
+++ Easy/21.py
@@ -0,0 +1,34 @@
+# 21. Merge Two Sorted Lists
+# Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+# Example:
+# Input: 1->2->4, 1->3->4
+# Output: 1->1->2->3->4->4
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        node = ListNode()
+        curr = node
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+
+        if l1:
+            curr.next = l1
+
+        if l2:
+            curr.next = l2
+        return node.next
